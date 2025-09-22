954ecfda56a1b6c1f21d8bc781d5e04e08a3a13f
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2025-09-22 21:45:41 +0700

    Update .gitignore to exclude generated files
    - Added *.g.dart and *.freezed.dart to prevent committing generated code
    - Removed existing generated .g.dart files from git tracking

--- lib/data/datasources/local/dao/clients_dao.g.dart
+++
@@ -1,8 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'clients_dao.dart';
-
-// ignore_for_file: type=lint
-mixin _$ClientsDaoMixin on DatabaseAccessor<AppDatabase> {
-  $ClientsTable get clients => attachedDatabase.clients;
-}
--- lib/data/datasources/local/dao/projects_dao.g.dart
+++
@@ -1,9 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'projects_dao.dart';
-
-// ignore_for_file: type=lint
-mixin _$ProjectsDaoMixin on DatabaseAccessor<AppDatabase> {
-  $ClientsTable get clients => attachedDatabase.clients;
-  $ProjectsTable get projects => attachedDatabase.projects;
-}
