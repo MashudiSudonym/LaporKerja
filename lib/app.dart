ad8e92bf4d79620e866f086b8f6ff340b06282b8
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2025-09-25 20:57:35 +0700

    feat: Refactor app structure to use ShadApp for improved theming and navigation

--- lib/app.dart
+++ lib/app.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lapor_kerja/presentation/providers/router/router_provider.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+    return ShadApp.custom(
+      themeMode: ThemeMode.system,
+      theme: ShadThemeData(
+        brightness: Brightness.light,
+        colorScheme: ShadOrangeColorScheme.light(),
       ),
-      routeInformationParser: ref.watch(routerProvider).routeInformationParser,
-      routeInformationProvider: ref
-          .watch(routerProvider)
-          .routeInformationProvider,
-      routerDelegate: ref.watch(routerProvider).routerDelegate,
+      darkTheme: ShadThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ShadOrangeColorScheme.dark(),
+      ),
+      appBuilder: (context) {
+        return MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          theme: Theme.of(context),
+          routeInformationParser: ref.watch(routerProvider).routeInformationParser,
+          routeInformationProvider: ref.watch(routerProvider).routeInformationProvider,
+          routerDelegate: ref.watch(routerProvider).routerDelegate,
+          builder: (context, child) {
+            return ShadAppBuilder(child: child!);
+          },
+        );
+      },
     );
   }
 }
